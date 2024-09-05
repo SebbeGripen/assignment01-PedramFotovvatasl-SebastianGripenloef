2493c2e26c56725e93612b2355d183ba8929b51c
OldSaltyCarb <lord.piggy@hotmail.com>
2024-09-05 15:08:39 +0200

    some assertions for the logout button

--- test-sebastian/testsuite01.spec.ts
+++ test-sebastian/testsuite01.spec.ts
@@ -11,6 +11,9 @@ test.describe('Test suite 01', () => {
     await loginPage.goto();
     await loginPage.performLogin(`${process.env.TEST_USERNAME}`,`${process.env.TEST_PASSWORD}`)
     await expect(page.getByRole('heading', { name: 'Tester Hotel Overview' })).toBeVisible();
+    await expect(dashboardPage.logoutButton).toBeEnabled();
+    await expect(dashboardPage.logoutButton).toHaveText('Logout');
+    await expect(dashboardPage.logoutButton).toBeVisible();
     await dashboardPage.performLogout();
     await expect(page.getByRole('heading', { name: 'Login' })).toBeVisible(); 
     await page.waitForTimeout(5000);   
